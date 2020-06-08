2fff77063bf147c41c0ca1d7bdb1ebdf0b3dda5b
“sandip5” <singhsandeep.singh97@gmail.com>
2020-06-08 17:43:32 +0530

    Gambler if won or lost 50% of stake, Would resign for the day

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -5,13 +5,23 @@ echo "Gambler start game with a stake of $100 every day and bet $1 for every gam
 stake=100;
 betAmountEachTime=1;
 isWin=0;
-randomCheck=$(( RANDOM%2 ))
-if [[ $isWin -eq $randomCheck ]]
-then
+
+maximumStakeAfterBetForDay=150;
+minimumStakeAfterBetForDay=50;
+totalWinTimeForDay=0;
+totalLooseTimeForDay=0;
+
+while [[ $stake -ge $minimumStakeAfterBetForDay && $stake -le $maximumStakeAfterBetForDay ]]
+do
+	randomCheck=$(( RANDOM%2 ))
+	if [[ $isWin -eq $randomCheck ]]
+	then
 		echo "Win $1"
 		stake=$(( $stake + $betAmountEachTime ))
-else
+		totalWinTimeForDay=$(( $totalWinTimeForDay + 1 ))
+	else
 		echo "Loose $1"
-		stake=$(( $stake +$betAmountEachTime ))
-fi
-
+		stake=$(( $stake - $betAmountEachTime ))
+		totalLooseTimeForDay=$(( $totalLooseTimeForDay + 1 ))
+	fi
+done
