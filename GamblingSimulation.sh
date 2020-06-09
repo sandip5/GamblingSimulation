59596a3d4ce5f99973de7ebf8619f3f19ea70a86
“sandip5” <singhsandeep.singh97@gmail.com>
2020-06-09 09:59:34 +0530

    Luckiest and Unluckiest calculation

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-declare -A DayReport
+declare -A WinDayReport
+declare -A LooseDayReport
 
-echo "Gambler start game with a stake of 100 every day and bet $1 for every game."
+echo "Gambler start game with a stake of 100 every day and bet 1 for every game."
 stake=100;
 betAmountEachTime=1;
 isWin=0;
@@ -10,8 +11,7 @@ maximumStakeAfterBetForDay=150;
 minimumStakeAfterBetForDay=50;
 totalWinTimeForDay=0;
 totalLooseTimeForDay=0;
-totalDayWon=0;
-totalDayLost=0;
+wallet=0;
 
 for(( dayCounter=1;dayCounter<=30;dayCounter++ ))
 do
@@ -29,11 +29,37 @@ do
 	done
 	if [[ $totalWinTimeForDay -ge $totalLooseTimeForDay ]]
 	then
-			echo "Day"$dayCounter" Won "$stake
+			wallet=$(( $wallet + 50 ))
+			WinDayReport["Day"$dayCounter]=$wallet
+			if(( wallet>maximumWin ))
+			then
+					maximumWin=$wallet;
+			fi
 	else
-			echo "Day"$dayCounter" Loose "$stake
+			wallet=$(( $wallet - 50 ))
+			LooseDayReport["Day"$dayCounter]=$wallet
+			if(( wallet<maximumLoose ))
+			then
+					maximumLoose=$wallet;
+			fi
 	fi
 stake=100;
 totalWinTimeForDay=0;
 totalLooseTimeForDay=0;
+done
+
+for key in ${!WinDayReport[@]}
+do
+	if [[ ${WinDayReport[$key]} -eq $maximumWin ]]
+	then
+		echo "Luckiest day for gambler --->"$key
+	fi
+done
+
+for key in ${!LooseDayReport[@]}
+do
+   if [[ ${LooseDayReport[$key]} -eq $maximumLoose ]]
+   then
+      echo "Unluckiest day for gambler --->"$key
+   fi
 done
